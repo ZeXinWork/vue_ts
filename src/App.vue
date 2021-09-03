3cee2e53d658107054f357cb8cd6d40369af4b8e
zhengzexin <zhengzexin@xmwefun.com>
2021-09-03 18:21:19 +0800

    feat: 添加头部navbar

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,66 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="container">
+    <global-header :user="user"></global-header>
+    <column-list :list="list"></column-list>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import ColumnList, { ColumnListProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/GlobalHeader.vue'
+const testData: ColumnListProps[] = [
+  {
+    id: 1,
+    title: 'title1专栏',
+    description: '这是test3的专栏，有一段简介,哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
+    avatar: 'https://img2.baidu.com/it/u=3355464299,584008140&fm=26&fmt=auto&gp=0.jpg'
+  },
+  {
+    id: 2,
+    title: 'title2专栏',
+    description: '这是test3的专栏，有一段简介,哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
+    avatar: 'https://img2.baidu.com/it/u=3355464299,584008140&fm=26&fmt=auto&gp=0.jpg'
+  },
+  {
+    id: 3,
+    title: 'title3专栏',
+    description: '这是test3的专栏，有一段简介,哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈'
+    // avatar: 'https://img2.baidu.com/it/u=3355464299,584008140&fm=26&fmt=auto&gp=0.jpg'
+  },
+  {
+    id: 4,
+    title: 'title4专栏',
+    description: '这是test3的专栏，有一段简介,哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
+    avatar: 'https://img2.baidu.com/it/u=3355464299,584008140&fm=26&fmt=auto&gp=0.jpg'
+  },
+  {
+    id: 5,
+    title: 'title5专栏',
+    description: '这是test3的专栏，有一段简介,哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈',
+    avatar: 'https://img2.baidu.com/it/u=3355464299,584008140&fm=26&fmt=auto&gp=0.jpg'
+  }
+]
+const currentUser: UserProps = {
+  isLogin: true,
+  name: '迪达拉',
+  id: 1
+}
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
+  components: { ColumnList, GlobalHeader },
+  setup() {
+    return {
+      list: testData,
+      user: currentUser
+    }
   }
 })
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+img {
+  width: 56px;
 }
 </style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,55 @@
+<template>
+  <div v-if="columnList && columnList.length > 0" class="row">
+    <div v-for="column of columnList" :key="column.id" class="col-4 mb-4">
+      <div class="card h-100 shadow-sm" style="width: 18rem;">
+        <div class="card-body text-center">
+          <img :src="column.avatar" class="rounded-circle border border-light w-25 my-3" :alt="column.title" />
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text text-left">
+            {{ column.description }}
+          </p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+// 定义ColumnList数组中每一项的对象属性
+export interface ColumnListProps {
+  id: number
+  avatar?: string
+  title: string
+  description: string
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnListProps[]>, // 定义list类型
+      require: true
+    }
+  },
+  setup(props) {
+    const columnList = props.list?.map(item => {
+      if (!item.avatar) {
+        item.avatar = require('@/assets/column.jpg')
+      }
+      return item
+    })
+    return {
+      columnList
+    }
+  }
+})
+</script>
+
+<style>
+img {
+  width: 100%;
+}
+</style>
